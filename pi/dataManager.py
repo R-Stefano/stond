bc04e0d1b570b4ab795a85653658ba035b606c55
Stefano Rosà <35814537+R-Stefano@users.noreply.github.com>
2022-01-28 00:25:12 +0000

    changes

--- pi/dataManager.py
+++ pi/dataManager.py
@@ -25,3 +25,24 @@ def sendData():
   response = requests.put(configs.apiUrl + "/api/devices/" + configs.deviceId + "/status", json = data)
   #if (response.status_code != 200):
   #  add('error', "Impossible processing request. Error {} | {}".format(response.status_code, response.text)))
+
+
+def displayData():
+  sensorsData = [
+      {'name': 'water_temperature', 'value': sensors.water.temperature, 'isWorking': sensors.water.temperatureSensorWorking},
+      {'name': 'water_level', 'value': sensors.water.level, 'isWorking': sensors.water.levelSensorWorking},
+      {'name': 'water_ph', 'value': sensors.water.ph, 'isWorking': sensors.water.phSensorWorking},
+      {'name': 'env_temperature', 'value': sensors.environment.temperature, 'isWorking': sensors.environment.temperatureHumiditySensorWorking},
+      {'name': 'env_humidity', 'value': sensors.environment.humidity, 'isWorking': sensors.environment.temperatureHumiditySensorWorking}
+  ]
+
+  actuatorsData = [
+      {'name': 'ventilation', 'status': controller.ventilation.status, 'isWorking': controller.ventilation.isWorking},
+      {'name': 'LED',         'status': controller.led.status, 'isWorking': controller.led.isWorking},
+  ]
+
+  for sensor in sensorsData:
+    print(sensor['name'], sensor['isWorking'], sensor['value'])
+
+  for actuator in actuatorsData:
+    print(actuator['name'], actuator['isWorking'], actuator['status'])
