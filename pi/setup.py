5f431e94d4a006159c3249a8cae0bf5993682877
Stefano Rosà <35814537+R-Stefano@users.noreply.github.com>
2022-02-06 22:35:09 +0000

    fixes

--- pi/setup.py
+++ pi/setup.py
@@ -19,16 +19,20 @@ def start():
     time.sleep(1)
     
   print("LIGHTS CHECKS")
+  message = "RELAY LED Should be ON - LIGHTS should be ON"
+  print(">>" + message)
   controller.led.controlLights("ON")
-  resp = input("RELAY LED Should be ON - LIGHTS should be ON (Enter to continue)")
+  resp = input("## Enter to continue ##")
   if (resp != ""):
-    testsFailed.append({"message": "RELAY LED Should be ON - LIGHTS should be ON"})
+    testsFailed.append({"message": message})
     return
 
+  message = "RELAY LED Should be OFF - LIGHTS should be OFF"
+  print(">>" + message)
   controller.led.controlLights("OFF")
-  resp = input("RELAY Should be OFF - LIGHTS should be OFF (Enter to continue)")
+  resp = input("## Enter to continue ##")
   if (resp != ""):
-    testsFailed.append({"message": "RELAY Should be OFF - LIGHTS should be OFF"})
+    testsFailed.append({"message": message})
     return
 
 
@@ -41,23 +45,26 @@ def start():
   - service fan on
   - service fan off
   '''
-  controller.ventilation.controlFanSpeed(0)
   message = "MAIN FAN Should be OFF"
-  resp = input(message + " (Enter to continue)")
+  print(">>" + message)
+  controller.ventilation.controlFanSpeed(0)
+  resp = input("## Enter to continue ##")
   if (resp != ""):
     testsFailed.append({"message": message})
     return
   
-  controller.ventilation.controlFanSpeed(25)
   message = "MAIN FAN Should be 25%"
-  resp = input(message + " (Enter to continue)")
+  print(">>" + message)
+  controller.ventilation.controlFanSpeed(25)
+  resp = input("## Enter to continue ##")
   if (resp != ""):
     testsFailed.append({"message": message})
     return
 
-  controller.ventilation.controlFanSpeed(100)
   message = "MAIN FAN Should be 100%"
-  resp = input(message + " (Enter to continue)")
+  print(">>" + message)
+  controller.ventilation.controlFanSpeed(100)
+  resp = input("## Enter to continue ##")
   if (resp != ""):
     testsFailed.append({"message": message})
     return
