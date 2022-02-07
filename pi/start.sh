752e1c4287c2ff5a1dd58b251019dfdaf0779b56
Stefano Rosà <35814537+R-Stefano@users.noreply.github.com>
2022-02-07 22:44:06 +0000

    changes

--- pi/start.sh
+++ pi/start.sh
@@ -1,10 +1,11 @@
 cd $(dirname "$(realpath $0)")
 
+echo "[AFRODITE STARTUP] Installing Libraries"
 pip3 install virtualenv
 source ./env/bin/activate
 pip3 install -r requirements.txt
 
-echo "TODO ENABLE I2C"
+echo "[AFRODITE STARTUP] TODO ENABLE I2C"
 
 ###TODO
 # sudo nano /boot/config.txt
@@ -19,12 +20,14 @@ echo "TODO ENABLE I2C"
 
 if grep -Fxq "/home/pi/stond/pi/start.sh" /etc/rc.local
 then
-    echo "Start Command already in the booter file. Start Routine"
+    echo "[AFRODITE STARTUP] Start Command already in the booter file. Start Routine"
     python3 main.py
 else
-    echo "Adding Start Command to the booter file & Reboot"
+    echo "[AFRODITE STARTUP] Adding Start Command to the booter file & Reboot"
     sudo chmod 777 /etc/rc.local
-    sudo sed -i 's/exit 0/\/home\/pi\/stond\/pi\/start.sh\nexit 0/' /etc/rc.local
+    sudo sed -i "s/exit 0//g" /etc/rc.local
+    echo "/home/pi/stond/pi/start.sh > /home/pi/stond/pi/startup_error.log" >> /etc/rc.local
+    echo "exit 0" >> /etc/rc.local
     sudo reboot
 fi
 
