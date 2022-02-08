7260f7d204c54d9f41c555d2ddc55393ed94544d
Stefano Rosà <35814537+R-Stefano@users.noreply.github.com>
2022-02-08 01:07:19 +0000

    setup file

--- pi/start.sh
+++ pi/start.sh
@@ -1,23 +1,3 @@
-cd $(dirname "$(realpath $0)")
-
-echo "[AFRODITE STARTUP] Installing Libraries"
-pip3 install virtualenv
-source ./env/bin/activate
-pip3 install -r requirements.txt
-
-echo "[AFRODITE STARTUP] TODO ENABLE I2C"
-
-###TODO
-# sudo nano /boot/config.txt
-# dtoverlay=w1-gpio
-# sudo modprobe w1-gpio
-# sudo modprobe w1-therm
-#
-#
-#
-#
-
-
 if grep -Fxq "/home/pi/stond/pi/start.sh" ~/.profile
 then
     echo "[AFRODITE STARTUP] Start Command already in the booter file. Start Routine"
