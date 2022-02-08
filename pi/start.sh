d2335326979141b8c2284c6235030accdb2d144e
Stefano Rosà <35814537+R-Stefano@users.noreply.github.com>
2022-02-08 18:21:11 +0000

    avoid script duplicate running

--- pi/start.sh
+++ pi/start.sh
@@ -1,15 +1,17 @@
+if $AFRODITE_IS_RUNNING == "yes"
+then
+    exit 1
+else
+    export AFRODITE_IS_RUNNING="yes"
+fi
+
 cd $(dirname "$(realpath $0)")
 source ./env/bin/activate
 
 if grep -Fxq "/home/pi/stond/pi/start.sh" ~/.profile
 then
-    if $AFRODITE_IS_RUNNING != "yes"
-    then
-        echo "[AFRODITE] Start Command already in the booter file. Start Routine"
-        python3 /home/pi/stond/pi/main.py
-    else
-        echo "[AFRODITE] Already Running"
-    fi
+    echo "[AFRODITE] Start Command already in the booter file. Start Routine"
+    python3 /home/pi/stond/pi/main.py
 else
     echo "[AFRODITE] Adding Start Command to the booter file & Reboot"
     echo "/home/pi/stond/pi/start.sh" >> ~/.profile
