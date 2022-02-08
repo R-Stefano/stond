9acebaac98e8578022be6d36b5a5dad87a2209e2
Stefano Rosà <35814537+R-Stefano@users.noreply.github.com>
2022-02-08 21:02:02 +0000

    update start script

--- pi/start.sh
+++ pi/start.sh
@@ -1,17 +1,15 @@
-if $AFRODITE_IS_RUNNING == "yes"
-then
-    exit 1
-else
-    export AFRODITE_IS_RUNNING="yes"
-fi
-
 cd $(dirname "$(realpath $0)")
 source ./env/bin/activate
 
 if grep -Fxq "/home/pi/stond/pi/start.sh" ~/.profile
 then
-    echo "[AFRODITE] Start Command already in the booter file. Start Routine"
-    python3 /home/pi/stond/pi/main.py
+    if ps ax | grep -v grep | grep "python3 main.py" > /dev/null
+    then
+        echo "[AFRODITE] Routing already running"
+    else
+        echo "[AFRODITE] Start Command already in the booter file. Start Routine"
+        python3 /home/pi/stond/pi/main.py
+    fi
 else
     echo "[AFRODITE] Adding Start Command to the booter file & Reboot"
     echo "/home/pi/stond/pi/start.sh" >> ~/.profile
