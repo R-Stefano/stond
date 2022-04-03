901573de511586a4b3d10db080af2c6d1929fc47
Stefano Rosà <35814537+R-Stefano@users.noreply.github.com>
2022-04-03 15:53:55 +0100

    fix

--- pi/controller.py
+++ pi/controller.py
@@ -11,7 +11,7 @@ class FanActuator():
         # Internal Variables
         self.BOTTOM_FAN_PIN = 12 # GPIO12 PWM0 (Physical PIN 32)
         self.TOP_FAN_PIN = 13 # GPIO13 PWM0 (Physical PIN 33)
-        self.PWM_FREQ = 25 # [kHz] 25kHz for Noctua PWM control
+        self.PWM_FREQ = 100 # [kHz] 25kHz for Noctua PWM control
         self.MIN_TEMP = 24
         self.MAX_TEMP = 28
         self.FAN_OFF = 0 
@@ -35,8 +35,7 @@ class FanActuator():
             self.fan_top = gpio.PWM(self.TOP_FAN_PIN, self.PWM_FREQ)
 
             self.fan_bottom.start(100) ## HARDCODED TO MAX FOR THE MOMENT
-            #self.fan_top.start(0)
-            self.fan_top.stop()
+            self.fan_top.start(0)
 
             self.isWorking = True
         except Exception as e:
