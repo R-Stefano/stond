85f3fa28fe92753c95a09f7ee67ecd4c0424b462
Stefano Rosà <35814537+R-Stefano@users.noreply.github.com>
2023-09-11 10:52:45 +0300

    updated deviceId

--- mobile-app/app/src/environments/environment.prod.ts
+++ mobile-app/app/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://stoned-api-f4lrk4qixq-nw.a.run.app/',
-  deviceId: 'c2836cef-887b-42a9-87a1-6d749b2852c0'
+  deviceId: 'acb21330-9533-458d-a5ce-25203551976c'
 };
