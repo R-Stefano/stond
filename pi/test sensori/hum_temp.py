cdd2dc996addab2463ad59fdcc875fdb0878f25a
GiuQua9 <giuseppe98.quaranta@gmail.com>
2023-10-14 18:58:44 +0200

    creazione test hum_temp

---
+++ pi/test sensori/hum_temp.py
@@ -0,0 +1,23 @@
+import board
+
+from adafruit_bme280 import basic as adafruit_bme280
+
+address = 0x76
+
+while(1):
+    print("[BME280] Reading Temp & Humidity\n")
+    try:
+        i2c = board.I2C()  # uses board.SCL and board.SDA
+        bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c, address)
+        try:
+            humidity = round(bme280.humidity, 2)
+            temperature = round(bme280.temperature, 2)
+        except Exception as e:
+            print("\n[BME280] Impossible Reading Temp & Humidity      ")
+            print(e)
+            humidity = 0
+            temperature = 0
+        print("Hum: "+humidity+" Temp= "+temperature+"\n")
+    except Exception as e:
+        print("\n[BME280] (start) Not working    ")
+        print(e)
