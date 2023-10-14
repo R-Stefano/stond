b4a15c86c326b44d64dc22e6d259fc637ddb9a6b
GiuQua9 <giuseppe98.quaranta@gmail.com>
2023-10-15 00:52:39 +0200

    ph

--- pi/test_sensori/ph.py
+++ pi/test_sensori/ph.py
@@ -1,4 +1,5 @@
 import busio, digitalio, board
+import time
 import sys
 sys.path.insert(1,'/home/pi/stond/pi')
 import main
@@ -8,13 +9,13 @@ import adafruit_mcp3xxx.mcp3008 as MCP
 WATER_PH_PIN = board.D5 # GPIO5 (Physical PIN 29)
 MCP3008_PH_PIN = 0 # PIN on the MCP3008 Module for the PH Sensor
 
+print("[E201-C-BNC] Reading PH")
 while(1):
     try:
         spi = busio.SPI(clock=board.SCLK, MISO=board.MISO, MOSI=board.MOSI) # create the spi bus
         cs = digitalio.DigitalInOut(WATER_PH_PIN) # PIN 29 GPIO5 - create the cs (chip select)
         mcp = MCP.MCP3008(spi, cs)
 
-        print("[E201-C-BNC] Reading PH")
         try:
             raw_ph = mcp.read(MCP3008_PH_PIN)
             # map 0-1024 to 0-14
@@ -31,4 +32,5 @@ while(1):
     except Exception as e:
         print("[MCP3008] (start) Not working. E201-C-BNC OR TDS Sensor not working")
         print(e)
+    time.sleep(2)
 
