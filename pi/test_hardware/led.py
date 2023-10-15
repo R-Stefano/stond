f01773270b2c010eef007a07b2e6386dddb01b5d
GiuQua9 <giuseppe98.quaranta@gmail.com>
2023-10-15 20:37:46 +0200

    add test led

--- pi/test_hardware/led.py
+++ pi/test_hardware/led.py
@@ -5,20 +5,20 @@ parser = argparse.ArgumentParser(description = 'Tests')
 parser.add_argument('action', type=str, help='The action to execute: sensors, led:on, hvac:off, hum:on', default="run", nargs='?')  
 args = parser.parse_args()
 
-HUMIDIFIER_GPIO_PIN = 25
-gpio.setmode (gpio.BCM)
-gpio.setup(HUMIDIFIER_GPIO_PIN, gpio.OUT, initial=gpio.LOW) # Start with HUMIDIFIER OFF
-try:
+LED_RELAY_GPIO_PIN = 16
 
+gpio.setmode (gpio.BCM)
+gpio.setup(LED_RELAY_GPIO_PIN, gpio.OUT, initial=gpio.LOW) # Start with HUMIDIFIER OFF
+
+try:
     if (args.action == "off"):
-        gpio.output(HUMIDIFIER_GPIO_PIN, gpio.HIGH)
-        print("[HUMIDIFIER] Start Setup OFF")
+        gpio.output(LED_RELAY_GPIO_PIN, gpio.HIGH)
+        print("[LED] Start Setup OFF")
     elif (args.action == "on"):
-        gpio.output(HUMIDIFIER_GPIO_PIN, gpio.LOW)
-        print("[HUMIDIFIER] Start Setup ON")
+        gpio.output(LED_RELAY_GPIO_PIN, gpio.LOW)
+        print("[LED] Start Setup ON")
     else:
         print("no action selected")
-
 except Exception as e:
-    print("[HUMIDIFIER] not working")
+    print("[LED] not working")
     print(e)
