b35496db26233ad92f0c8a2a80dcec4ee58e7357
GiuQua9 <giuseppe98.quaranta@gmail.com>
2023-10-15 20:25:56 +0200

    mist maker

--- pi/test_hardware/mist_maker.py
+++ pi/test_hardware/mist_maker.py
@@ -6,15 +6,17 @@ parser.add_argument('action', type=str, help='The action to execute: sensors, le
 args = parser.parse_args()
 
 HUMIDIFIER_GPIO_PIN = 25
+while(1):
+    try:
+        print("[HUMIDIFIER] Start Initial Setup")
 
-try:
-    print("[HUMIDIFIER] Start Initial Setup")
+        if (args.action == "off"):
+            gpio.output(HUMIDIFIER_GPIO_PIN, gpio.HIGH)
+        elif (args.action == "on"):
+            gpio.output(HUMIDIFIER_GPIO_PIN, gpio.LOW)
+        else:
+            print("no action selected")
 
-    if (args.action == "off"):
-        gpio.output(HUMIDIFIER_GPIO_PIN, gpio.HIGH)
-    elif (args.action == "on"):
-        gpio.output(HUMIDIFIER_GPIO_PIN, gpio.LOW)
-
-except Exception as e:
-    print("[HUMIDIFIER] not working")
-    print(e)
+    except Exception as e:
+        print("[HUMIDIFIER] not working")
+        print(e)
