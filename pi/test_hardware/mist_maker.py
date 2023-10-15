07b199e7264b23ded64965b09bf971da72c6c319
GiuQua9 <giuseppe98.quaranta@gmail.com>
2023-10-15 20:24:08 +0200

    add test mist maker

---
+++ pi/test_hardware/mist_maker.py
@@ -0,0 +1,20 @@
+from RPi import GPIO as gpio
+
+import argparse
+parser = argparse.ArgumentParser(description = 'Tests')
+parser.add_argument('action', type=str, help='The action to execute: sensors, led:on, hvac:off, hum:on', default="run", nargs='?')  
+args = parser.parse_args()
+
+HUMIDIFIER_GPIO_PIN = 25
+
+try:
+    print("[HUMIDIFIER] Start Initial Setup")
+
+    if (args.action == "off"):
+        gpio.output(HUMIDIFIER_GPIO_PIN, gpio.HIGH)
+    elif (args.action == "on"):
+        gpio.output(HUMIDIFIER_GPIO_PIN, gpio.LOW)
+
+except Exception as e:
+    print("[HUMIDIFIER] not working")
+    print(e)
