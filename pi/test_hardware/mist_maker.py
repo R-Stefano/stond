2cc9a315c40445583f14b3f330011095cc9c2341
GiuQua9 <giuseppe98.quaranta@gmail.com>
2023-10-15 20:32:31 +0200

    mist

--- pi/test_hardware/mist_maker.py
+++ pi/test_hardware/mist_maker.py
@@ -8,18 +8,16 @@ args = parser.parse_args()
 HUMIDIFIER_GPIO_PIN = 25
 gpio.setmode (gpio.BCM)
 gpio.setup(HUMIDIFIER_GPIO_PIN, gpio.OUT, initial=gpio.LOW) # Start with HUMIDIFIER OFF
-while(1):
-    try:
-        print("[HUMIDIFIER] Start Initial Setup")
+print("[HUMIDIFIER] Start Setup")
+try:
 
-        if (args.action == "off"):
-            gpio.output(HUMIDIFIER_GPIO_PIN, gpio.HIGH)
-        elif (args.action == "on"):
-            gpio.output(HUMIDIFIER_GPIO_PIN, gpio.LOW)
-        else:
-            print("no action selected")
+    if (args.action == "off"):
+        gpio.output(HUMIDIFIER_GPIO_PIN, gpio.HIGH)
+    elif (args.action == "on"):
+        gpio.output(HUMIDIFIER_GPIO_PIN, gpio.LOW)
+    else:
+        print("no action selected")
 
-    except Exception as e:
-        print("[HUMIDIFIER] not working")
-        print(e)
-    time.sleep(1)
+except Exception as e:
+    print("[HUMIDIFIER] not working")
+    print(e)
